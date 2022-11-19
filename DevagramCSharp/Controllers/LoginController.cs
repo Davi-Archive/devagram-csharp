b67109ca88f702fc447a655ab052f0db644051db
d1av <davi4alves@gmail.com>
2022-11-19 13:59:50 -0300

    feat: add login code

--- DevagramCSharp/Controllers/LoginController.cs
+++ DevagramCSharp/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevagramCSharp.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevagramCSharp.Controllers
 {
@@ -8,19 +10,29 @@ namespace DevagramCSharp.Controllers
     {
 
 
+        private readonly ILogger<LoginController> _logger;
 
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
 
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult EfetuarLogin([FromBody] LoginController loginrequisicao)
+        public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto loginrequisicao)
         {
             try
             {
-
+                throw new ArgumentException("Erro ao preencher os dados");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
+                _logger.LogError("Ocorreu um erro no login: " + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorRespostaDto()
+                {
+                    Descricao = 'Ocorreu um erro ao fazer o login',
+                    Status = StatusCodes.Status500InternalServerError
+                })
             }
         }
     }
