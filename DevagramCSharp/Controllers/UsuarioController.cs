29035334868cbd5376f797fb94b8f9c473a6ffbb
d1av <davi4alves@gmail.com>
2022-11-19 15:38:57 -0300

    feat: add authentication on get user

---
+++ DevagramCSharp/Controllers/UsuarioController.cs
@@ -0,0 +1,46 @@
+﻿using DevagramCSharp.Dtos;
+using DevagramCSharp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevagramCSharp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        public readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult ObterUsuario()
+        {
+            try
+            {
+                Usuario usuario = new Usuario()
+                {
+                    Email = "daniel@devaria.com.br",
+                    Nome = "Daniel",
+                    Id = 100
+                };
+
+                return Ok(usuario);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Ocorreu um erro ao obter o usuário");
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorRespostaDto()
+                {
+                    Descricao = "Ocorreu o seguinter erro: " + ex.Message,
+                    Status = StatusCodes.Status500InternalServerError
+                });
+            }
+        }
+    }
+}
