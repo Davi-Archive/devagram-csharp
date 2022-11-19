11fc3b2c3c53aeb0da19a9a6f429ae1c85748154
d1av <davi4alves@gmail.com>
2022-11-19 16:12:06 -0300

    feat: add migration
    using the command dotnet-ef migration myFirstMigration
    it did add the migration folder on the project base directory

--- DevagramCSharp/Program.cs
+++ DevagramCSharp/Program.cs
@@ -1,6 +1,8 @@
 using System.Text;
 using DevagramCSharp;
+using DevagramCSharp.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DevagramContext>(option => option.UseSqlServer(connectionString));
 
 var chaveCriptogragia = Encoding.ASCII.GetBytes(ChaveJWT.ChaveSecreta);
 builder.Services.AddAuthentication(auth =>
